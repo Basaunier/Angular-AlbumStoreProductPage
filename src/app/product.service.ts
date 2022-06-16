e6f1f8113fed04a78d9c20b13aaaf326602795e2
Blake Saunier <basaunierjr@gmail.com>
2022-06-16 12:38:37 -0400

    Update 5

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,12 +3,14 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs';
 import { Album } from './album';
+import {Product} from './product';
 
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl: string = "../assets/album.json";
+  private _productsUrl: string = "../assets/products.json";
 
   constructor(private _http: Http) { }
 
@@ -16,5 +18,7 @@ export class ProductService {
   getAlbum(id: number): Observable<Album>{
     return this._http.get(this._albumUrl).map(response => <Album>response.json());
   }
-
+  getProducts(): Observable<Product[]>{
+    return this._http.get(this._productsUrl).map(response => <Product[]>response.json())
+  }
 }
